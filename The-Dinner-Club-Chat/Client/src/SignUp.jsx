165b67184c4b75c1f5984d12a8eddf76e45df2b3
MikeyLpx <MichaelLpz85301@gmail.com>
2023-11-27 18:54:45 -0700

    added sign up and chakra

--- The-Dinner-Club-Chat/Client/src/SignUp.jsx
+++ The-Dinner-Club-Chat/Client/src/SignUp.jsx
@@ -1,24 +1,47 @@
-import { useState } from "react"
+import React, { useState } from "react";
+import { Button, Heading, AspectRatio, Image} from '@chakra-ui/react';
 
 export default function SignUp() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    return (
-        <div>
-            <form>
-                <input value={username}
-                    onChange={ev => setUsername(ev.target.value)}
-                    Class="SignupInput"
-                    type="text"
-                    placeholder="Username" />
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-                <input value={password}
-                onChange={ev => setPassword(ev.target.value)}
-                class="SignupInput" 
-                type="text" 
-                placeholder="Password" />
-                <button>SignUp</button>
-            </form>
-        </div>
-    )
+    const handleSubmit = (event) => {
+        event.preventDefault();
+       
+    };
+
+    return (
+        <>
+            <Heading>
+                Dinner Table Chats
+            </Heading>
+            <AspectRatio maxW='400px' ratio={4 / 3}>
+            <Image src='dinner tables img.jpg' alt='naruto' objectFit='cover' />
+            </AspectRatio>
+            <div>
+                <form onSubmit={handleSubmit}>
+            
+                        <input
+                            value={username}
+                            onChange={ev => setUsername(ev.target.value)}
+                            className="SignupInput"
+                            type="text"
+                            placeholder="Username"
+                        />
+
+                
+                        <input
+                            value={password}
+                            onChange={ev => setPassword(ev.target.value)}
+                            className="SignupInput"
+                            type="password" // Change to password type for secure input
+                            placeholder="Password"
+                        />
+                    
+
+                    <Button colorScheme = 'teal' type="submit">Sign Up</Button>
+                </form>
+            </div>
+        </>
+    );
 }
